6f8a9bc79db7f3fd5096a84e2427293822de3247
sebastian <sebastiang300.sg@gmail.com>
2024-02-20 17:30:59 -0400

    ok

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,17 +7,24 @@ import { CatalogoModule } from './catalogo/catalogo.module';
 import { NotificacionesModule } from './notificaciones/notificaciones.module';
 import { NoticoModule } from './notico/notico.module';
 
+
 @Module({
-  imports: [TypeOrmModule.forRoot({
-    type: 'mysql',
-    host: 'localhost',
-    port: 3306,
-    username: 'root', // root
-    password: 'root', // root
-    database: 'notifications', // notificaciones
-    entities: [__dirname + '/**/*.entity{.ts,.js}'],
-    synchronize: true,
-  }),DocumentoModule, CatalogoModule, NotificacionesModule, NoticoModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'notifications',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    DocumentoModule,
+    CatalogoModule,
+    NotificacionesModule,
+    NoticoModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
